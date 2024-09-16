40e85a6bd0f1eab8d7dcb2dce05c41ef356f2da1
AndreySaez <theleji.andrey@gmail.com>
2024-09-16 10:18:44 +0400

    сделал карточку товара, переход на нее по клику на элемент списка

--- app/src/main/java/com/example/fakeshop/MainActivity.kt
+++ app/src/main/java/com/example/fakeshop/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.fakeshop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.fakeshop.productlist.presentation.view.productslist.ProductsListFragment
+import com.example.fakeshop.registration.presentation.view.RegistrationFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.main, ProductsListFragment()).commit()
+                .add(R.id.main, RegistrationFragment()).commit()
         }
     }
 }
