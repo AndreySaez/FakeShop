b12cb4d65852f83e0b22d30e9208877c473621b5
AndreySaez <theleji.andrey@gmail.com>
2024-11-12 13:58:28 +0400

    исправил ошибку

--- product-details/impl/src/main/java/com/example/productdetails/presentation/ProductDetailsFragment.kt
+++ product-details/impl/src/main/java/com/example/productdetails/presentation/ProductDetailsFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.addCallback
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -56,14 +55,5 @@ internal class ProductDetailsFragment : Fragment() {
         }
     }
 
-
-    companion object {
-
-        private const val PRODUCT_KEY = "Product"
-        fun newInstance(product: Product): ProductDetailsFragment {
-            return ProductDetailsFragment().apply {
-                arguments = bundleOf(PRODUCT_KEY to product)
-            }
-        }
-    }
+    private val PRODUCT_KEY = "Product"
 }
