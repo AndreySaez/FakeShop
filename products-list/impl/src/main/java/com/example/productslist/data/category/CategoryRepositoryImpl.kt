97c41cdc3cdae155e0991083c5f577b5826272e5
AndreySaez <theleji.andrey@gmail.com>
2024-11-13 17:59:18 +0400

    Вынес retrofit запрос с категориями товаров в модуль соответствующей фичи

--- products-list/impl/src/main/java/com/example/productslist/data/category/CategoryRepositoryImpl.kt
+++ products-list/impl/src/main/java/com/example/productslist/data/category/CategoryRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.example.productslist.data.category
 
-import com.example.coremodule.ApiInterface
 import com.example.coremodule.productlist.Category
 import com.example.productslist.domain.category.CategoryRepository
 import kotlinx.coroutines.Dispatchers
@@ -8,10 +7,10 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CategoryRepositoryImpl @Inject constructor(
-    private val apiInterface: ApiInterface,
+    private val categoriesApi: CategoriesApi,
     private val mapper: CategoryMapper
 ) : CategoryRepository {
     override suspend fun getCategories(): List<Category> = withContext(Dispatchers.IO) {
-        apiInterface.getCategories().map(mapper::toCategory)
+        categoriesApi.getCategories().map(mapper::toCategory)
     }
 }
